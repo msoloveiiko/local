3e1c15d3f987b08d72ffeb33c04b52048c1964c8
msoloveiiko <bonnie.solo.ma@gmail.com>
2022-05-25 13:22:21 +0300

    Add controller

--- web/modules/custom/bonnie/src/Controller/BonnieController.php
+++ web/modules/custom/bonnie/src/Controller/BonnieController.php
@@ -1,1 +1,28 @@
+<?php
 
+namespace Drupal\bonnie\Controller;
+
+/**
+ * Provides route responses for the bonnie module.
+ */
+class BonnieController {
+
+  /**
+   * Returns a page.
+   *
+   * @return array
+   *   A renderable array.
+   */
+  public function content() {
+    return [
+      '#type' => 'html_tag',
+      '#tag' => 'h1',
+      '#value' => 'Hello! You can add here a photo of your cat.',
+      '#attributes' => [
+        'class' => ['bonnie-title'],
+      ],
+
+    ];
+  }
+
+}
